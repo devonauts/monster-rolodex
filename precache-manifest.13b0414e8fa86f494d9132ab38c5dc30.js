25a84247769ba81a23149c372d2b68ed549d34e9
Michael Urresta <michaelurrestap@hotmail.com>
2020-06-26 14:16:01 -0600

    Updates

---
+++ precache-manifest.13b0414e8fa86f494d9132ab38c5dc30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e73ac18fb006034e6cb8ce72434a8751",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "49e4290d5eff04b9a9b4",
+    "url": "/monster-rolodex/static/css/main.5d08e55b.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49e4290d5eff04b9a9b4",
+    "url": "/monster-rolodex/static/js/main.ac556b38.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
